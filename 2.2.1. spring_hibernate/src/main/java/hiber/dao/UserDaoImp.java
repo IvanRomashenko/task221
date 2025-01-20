5fcf3672130aa346990d19d9f3d9eaf9c9b22d49
Ivan Romashenko <romashenko.smart@gmail.com>
2025-01-20 17:53:50 +0300

    fixed problem n+1

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class UserDaoImp implements UserDao {
@@ -30,15 +31,30 @@ public class UserDaoImp implements UserDao {
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory
+                .getCurrentSession()
+                .createQuery("select u from User u left join fetch u.car",User.class);
         return query.getResultList();
     }
 
     @Override
     public User getTheCarUserByModelAndSeries(String model, int series) {
-        String hql = "select u from User u where u.car.model=:model and u.car.series=:series";
-        Query query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
-        query.setParameter("model", model).setParameter("series", series);
-        return (User) query.setMaxResults(1).getSingleResult();
+//        String hql = "select u from User u where u.car.model=:model and u.car.series=:series";
+//        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+//        query.setParameter("model", model).setParameter("series", series);
+//        return query.setMaxResults(1).getSingleResult();
+
+        String hql = "select u from User u join fetch u.car c where c.model=:model and c.series=:series";
+        List<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class)
+                .setParameter("model",model)
+                .setParameter("series",series)
+                .list();
+        if(query.isEmpty()){
+            System.out.println("car not found");
+            return null;
+        }else {
+            System.out.println("car found");
+            return query.get(0);
+        }
     }
 }
